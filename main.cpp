6c7ac55878254faf698789a5f3a34fc6e517448c
SasuKe_Gi1Hub <sasukeuchihateam14@gmail.com>
2024-08-12 12:34:41 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+#include <cstdlib>
 #include <iostream>
 
 int main (int argc, char *argv[]) {
     if (argc < 2) {
-    
+        std::cerr << "Error\n<./exec> <src_file>\n";
+        exit(-1);
     }
+
+    std::cout << "Reading from : " << argv[1] << "\n";
     return 0;
 }
