ea603aa5f33eb4e7556f3efc552d5508b0738fa0
SasuKe_Gi1Hub <sasukeuchihateam14@gmail.com>
2024-08-14 13:00:53 +0530

    add lexer.h

---
+++ headers/lexer.h
@@ -0,0 +1,19 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    IDENTIFIER,
+    NUMBER,
+    SYMBOL,
+    END_OF_FILE,
+    UNKNOWN
+};
+
+class Lexer {
+
+};
+
+#endif // !LEXER_H
